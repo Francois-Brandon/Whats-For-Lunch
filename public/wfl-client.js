df057434eb129a730be3fd375d9b7061bfee0940
Brandon Francois <bafrancois@gmail.com>
2018-12-01 22:25:51 -0700

    working on html ui and interface

--- public/wfl-client.js
+++ public/wfl-client.js
@@ -6,6 +6,7 @@ function restaurantSearch() {
     $.get("/restaurants", {location:location, radius:radius,categories:categories}, function(data) {
         
         var parsedData = JSON.parse(data);
+        console.log(parsedData);
         $("#ulScriptures").append("<li>" + parsedData[0].name + "</li>");
         $("#ulScriptures").append("<li>" + parsedData[1].name + "</li>");
         $("#ulScriptures").append("<li>" + parsedData[2].name + "</li>");
